e4a1516d6d897adf41e685a7df288bda7b5bd74f
Stephanie Shea <stephcshea@gmail.com>
2018-01-19 21:13:28 +0000

    meanstack testing initial commit

---
+++ meanSkeleton/app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.get("/", function(request, response) {
+    response.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(process.env.PORT);
